87c6f783c613bdbd04f654ebb6228e3f893ad4b6
Eko <alpcelik2013@gmail.com>
2026-05-16 10:32:51 +0300

    fgdf

--- server/passport.js
+++ server/passport.js
@@ -51,7 +51,7 @@ passport.use(
   new RobloxStrategy(
     {
       clientID: process.env.ROBLOX_OAUTH_CLIENT_ID || process.env.ROBLOX_CLIENT_ID || "6748474744887863615",
-      clientSecret: process.env.ROBLOX_OAUTH_CLIENT_SECRET || process.env.ROBLOX_CLIENT_SECRET || "RBX-4cu22Y7nHEGVVnrep29VdQoTsZQxHFrhhObPdjejfsKDr1f4P9K1DONn5VpcHxLL",
+      clientSecret: process.env.ROBLOX_OAUTH_CLIENT_SECRET || process.env.ROBLOX_CLIENT_SECRET || "RBX-4cu22Y7nHEGVVnrep29VdXblXPmUj6GVE-8vge5zjmw-cMhX37JWF7PlQ9D7-bbe",
       callbackURL: `${BASE_URL}/auth/roblox/callback`,
       scope: ["openid", "profile"],
       pkce: true,
